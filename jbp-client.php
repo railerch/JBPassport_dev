1fe92315407bf0abccced41214fac77edc1a766b
Railer Chalbaud <railer.chalbaud@gmail.com>
2022-11-27 18:34:07 -0400

    UPDATE: nueva interface

--- jbp-client.php
+++ jbp-client.php
@@ -16,36 +16,39 @@ if (!$_SESSION['cliente']) {
 </head>
 
 <body>
-    <div id="client-ui" class="container col-md-6 rounded-bottom py-3 my-md-3">
+    <div id="admin-ui" class="container col-md-6 rounded-bottom py-3 my-md-3">
         <!-- ESTATUS DE CUENTA -->
-        <header class="d-flex justify-content-center">
-            <div class="col-3">
-                <img id="login-logo" class="img-fluid" src="img/logo.png" alt="JBPassport">
-            </div>
-            <div class="col-md-4 d-flex flex-column justify-content-center">
-                <h2>JBPassport</h2>
-                <p>Estado de Cuenta de cliente<br>Profit PLUS Movil Access</p>
-            </div>
+        <header class="text-center mx-auto">
+            <img id="logo" class="img-fluid pb-2" src="img/logo.png" alt="JBPassport">
+            <p>
+                Estado de Cuenta de cliente
+                <br>
+                Carta CE Servicios Internacionales
+            </p>
         </header>
         <hr>
         <main class="">
-            <h3 id="tipo-usuario">Cliente: <span id="id-usuario">00269</span></h3>
+            <h3 id="tipo-usuario"><i class="icon-user"></i>ID Cliente: <span id="id-usuario">32563</span></h3>
             <hr>
 
-            <div id="montos" class="rounded mt-3 p-2">
+            <!-- RESUMEN DE CUENTA -->
+            <div id="montos" class="rounded mt-3 p-2" style="background-color:lightblue">
                 <p class="mb-1"><b>Fecha:</b> <span id="fecha"></span></p>
                 <p class="mb-1"><b>Saldo actual:</b> Bs. <span id="saldo-actual-bs">3,459.2</span> | $<span id="saldo-actual-usd">1,250.00</span></p>
                 <p class="mb-1"><b>Ultimo pago:</b> <span id="ultimo-pago">Credito 7 días</span></p>
                 <!-- Ultimo pago: Prepago, Credito 7 días, Credito 10 días -->
-                <p><b>Prepago sugerido:</b> $<span id="prepago-sugerido">125.00</span></p>
+                <p class="mb-1"><b>Pago sugerido:</b> $<span id="prepago-sugerido">125.00</span></p>
+                <p class="mb-1"><b>Pago pendiente:</b> $<span id="prepago-sugerido">250.00</span></p>
             </div>
 
+            <!-- TABLAS -->
             <div class="py-3">
                 <div class="accordion" id="registros-tbl">
+                    <!-- Últimas recargas -->
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingOne">
                             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#fuelling-uplift" aria-expanded="true" aria-controls="collapseOne">
-                                <h4><i class="icon-tint"></i> Ultimas recargas</h4>
+                                <h4><i class="icon-tint"></i> Últimas recargas</h4>
                             </button>
                         </h2>
                         <div id="fuelling-uplift" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#registros-tbl">
@@ -57,7 +60,7 @@ if (!$_SESSION['cliente']) {
                                                 <th>BLT</th>
                                                 <th>Fecha</th>
                                                 <th>Localidad</th>
-                                                <th>Volumen (gal)</th>
+                                                <th>UOM</th>
                                                 <th>Tale</th>
                                             </tr>
                                         </thead>
@@ -69,24 +72,17 @@ if (!$_SESSION['cliente']) {
                                                 <td>350</td>
                                                 <td>YV-52365</td>
                                             </tr>
-                                            <tr>
-                                                <td>8569</td>
-                                                <td>14/11/2022</td>
-                                                <td>La Guaira</td>
-                                                <td>250</td>
-                                                <td>YV-87456</td>
-                                            </tr>
                                         </tbody>
                                     </table>
                                 </div>
-
                             </div>
                         </div>
                     </div>
+                    <!-- Ultimos pagos -->
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingOne">
                             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#estatus-financiero" aria-expanded="true" aria-controls="collapseOne">
-                                <h4><i class="icon-money"></i> Ultimos pagos</h4>
+                                <h4><i class="icon-list-numbered"></i> Últimos pagos</h4>
                             </button>
                         </h2>
                         <div id="estatus-financiero" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#registros-tbl">
@@ -98,6 +94,7 @@ if (!$_SESSION['cliente']) {
                                                 <th>Fecha</th>
                                                 <th>Referencia</th>
                                                 <th>Monto</th>
+                                                <th>Tipo pago</th>
                                             </tr>
                                         </thead>
                                         <tbody>
@@ -105,42 +102,135 @@ if (!$_SESSION['cliente']) {
                                                 <td>12/05/2022</td>
                                                 <td>587458965</td>
                                                 <td>45.26</td>
+                                                <td>CASH</td>
                                             </tr>
                                         </tbody>
                                     </table>
                                 </div>
-
                             </div>
                         </div>
                     </div>
+                    <!-- Solicitud de combustible -->
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#solicitud-combustible" aria-expanded="true" aria-controls="collapseOne">
+                                <h4><i class="icon-gauge"></i> Solicitud de combustible</h4>
+                            </button>
+                        </h2>
+                        <div id="solicitud-combustible" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#registros-tbl">
+                            <div class="accordion-body">
+                                <form id="solicitud-combustible-frm" class="d-grid gap-2">
+                                    <input type="text" name="nombre" class="form-control" placeholder="Nombre">
+                                    <input type="text" name="empresa" class="form-control" placeholder="Compañia">
+                                    <input type="text" name="email" class="form-control" placeholder="Email">
+                                    <input type="text" name="telefono" class="form-control" placeholder="Teléfono">
+                                    <input type="text" name="ruta-vuelo" class="form-control" placeholder="Ruta de vuelo">
+                                    <div class="input-group">
+                                        <span class="input-group-text">Fecha de vuelo</span>
+                                        <input class="form-control" type="date" name="fecha-vuelo" class="form-control" placeholder="Fecha de vuelo">
+                                    </div>
+                                    <input type="text" name="eta" class="form-control" placeholder="ETA">
+                                    <input type="text" name="etd" class="form-control" placeholder="ETD">
+                                    <select class="form-select form-select" name="general">
+                                        <option selected>General</option>
+                                        <option value="">Opcion 01</option>
+                                        <option value="">Opcion 02</option>
+                                        <option value="">Opcion 03</option>
+                                    </select>
+                                    <input type="number" name="combustible-estimado" class="form-control" placeholder="Combustible estimado">
+                                    <select class="form-select form-select" name="servidio">
+                                        <option selected>Tipo de servicio</option>
+                                        <option value="">Opcion 01</option>
+                                        <option value="">Opcion 02</option>
+                                        <option value="">Opcion 03</option>
+                                    </select>
+                                    <button type="submit" class="btn btn-outline-secondary mt-2"><i class="icon-ok"></i> Enviar</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Actualizar datos de cliente -->
                     <div class="accordion-item">
                         <h2 class="accordion-header" id="headingOne">
                             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#datos-usuario" aria-expanded="true" aria-controls="collapseOne">
-                                <h4><i class="icon-arrows-cw"></i> Actualizar datos</h4>
+                                <h4><i class="icon-id-card"></i> Datos de cliente</h4>
                             </button>
                         </h2>
                         <div id="datos-usuario" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#registros-tbl">
                             <div class="accordion-body">
                                 <div id="aviso-usuario"></div>
-                                <div class="mb-3">
-                                    <label for="fecha-nac" class="form-label">Fecha de nacimiento</label>
-                                    <input type="date" class="form-control" name="fecha-nac" id="fecha-nac" aria-describedby="helpId" placeholder="">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="direccion" class="form-label">Dirección</label>
-                                    <input type="text" class="form-control" name="direccion" id="direccion" aria-describedby="helpId" placeholder="">
-                                </div>
-                                <button type="button" id="actualizar-datos" class="btn btn-outline-primary"><i class="icon-ok"></i> Actualizar</button>
+                                <form id="actualizar-datos-frm" class="d-grid gap-2">
+                                    <div class="input-group">
+                                        <span class="input-group-text">Nombre cliente</span>
+                                        <input type="text" class="form-control" name="nombre-cli" id="nombre-cli">
+                                    </div>
+                                    <div class="input-group">
+                                        <span class="input-group-text">Cedula/RIF</span>
+                                        <input type="text" class="form-control" name="cedula-rif" id="cedula-rif">
+                                    </div>
+                                    <div class="input-group">
+                                        <span class="input-group-text">Dirección</span>
+                                        <input type="text" class="form-control" name="direccion" id="direccion">
+                                    </div>
+                                    <button type="button" id="actualizar-datos" class="btn btn-outline-secondary"><i class="icon-ok"></i> Actualizar</button>
+                                </form>
                             </div>
                         </div>
                     </div>
-                </div>
+                    <!-- Informacion de interes  -->
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#informacion-interes" aria-expanded="true" aria-controls="collapseOne">
+                                <h4><i class="icon-info-circled"></i> Información de interes </h4>
+                            </button>
+                        </h2>
+                        <div id="informacion-interes" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#registros-tbl">
+                            <div class="accordion-body">
 
-                <div class="text-center pt-3">
-                    <a href="main-ctrl.php?cerrarSesion=true" id="logout-btn" class="btn btn-outline-primary"><i class="icon-logout"></i> Cerrar sesión</a>
-                </div>
-            </div>
+                                <p class="mt-3 mb-1"><b>Dirección de pagos</b></p>
+                                <hr class="mt-0">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ea aliquam numquam rem eos ipsam, laboriosam quaerat architecto! Est, delectus.</p>
 
+                                <p class="mt-3 mb-1"><b>Noticias</b></p>
+                                <hr class="mt-0">
+
+                                <!-- Noticias -->
+                                <div class="card mb-2">
+                                    <div class="card-body d-flex p-0 py-2">
+                                        <div class="col-3">
+                                            <img class="card-img-top" src="img/logo01.png" alt="Title">
+                                        </div>
+                                        <div class="col-9 text-muted p-2">
+                                            <h4 class="card-title">Noticia 1</h4>
+                                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="card mb-2">
+                                    <div class="card-body d-flex p-0 py-2">
+                                        <div class="col-3">
+                                            <img class="card-img-top" src="img/logo01.png" alt="Title">
+                                        </div>
+                                        <div class="col-9 text-muted p-2">
+                                            <h4 class="card-title">Noticia 2</h4>
+                                            <p class="card-text">
+                                                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                                                Odit repudiandae eum porro quod laborum veritatis.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- CERRAR SESION -->
+                    <div class="text-center pt-3">
+                        <a href="main-ctrl.php?cerrarSesion=true" id="logout-btn" class="btn btn-outline-dark"><i class="icon-logout"></i> Cerrar sesión</a>
+                    </div>
+                </div>
         </main>
     </div>
     <footer>
--- login.php
+++ login.php
@@ -7,19 +7,61 @@
 </head>
 
 <body>
+    <!-- FORMULARIO SESION -->
     <div class="container d-flex justify-content-center align-items-center">
         <div class="col-md-3 text-center shadow border rounded p-3" style="background-color:#333;color:#fff">
-            <img id="login-logo" class="mb-3" src="img/logo.png" alt="JBPassport" style="max-width:60%">
-            <h4>JBPassport</h4>
-            <p class="card-text">Sistema de información y recarga</p>
+            <div id="login-header" class="d-flex flex-column justify-content-center align-items-center p-2 mb-3">
+                <img id="login-logo" class="mb-3" src="img/logo.png" alt="JBPassport" style="max-width:60%">
+                <p class="card-text"><b>Nuestro servicio al alcence de tus manos</b></p>
+            </div>
             <div id="aviso-usuario"></div>
             <div class="mb-3">
-                <input type="text" name="usuario" id="usuario" class="form-control" placeholder="Nombre de usuario" aria-describedby="helpId">
-                <input type="password" name="clave" id="clave" class="form-control mt-2" placeholder="Clave" aria-describedby="helpId">
-                <button id="login-btn" class="btn btn-outline-primary mt-3"><i class="icon-login"></i> Iniciar sesión</button>
+                <input type="text" name="usuario" id="usuario" class="form-control" placeholder="ID usuario">
+                <input type="password" name="clave" id="clave" class="form-control mt-2" placeholder="Clave">
+                <div class="d-grid gap-2">
+                    <button id="login-btn" class="btn btn-outline-secondary mt-3"><i class="icon-login"></i> Iniciar sesión</button>
+                    <button class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#formulario-membresia">Solicitar membresía <b>JBP Passport Club</b></button>
+                </div>
             </div>
         </div>
     </div>
+
+    <!-- FORMULARIO MEMBRESIA -->
+    <!-- Modal trigger button -->
+    <div class="modal fade" id="formulario-membresia" tabindex="-1" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitleId"><i class="icon-user-circle-o"></i> Membresía JBP</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="formulario-membresia">
+                        <p>
+                            Únase a nuestro equipo con mas de 2500 miembros con solo rellenar el formulario a continuación.
+                            <br>
+                            Uno de nuestros ejecutivos lo contactara en la brevedad posible para presentarle nuestros servicios y beneficios.
+                        </p>
+                        <div class="d-flex flex-wrap d-grid gap-2">
+                            <input type="text" name="nombre" class="form-control" placeholder="Nombre">
+                            <input type="text" name="apellido" class="form-control" placeholder="Apellido">
+                            <input type="text" name="email" class="form-control" placeholder="Email">
+                            <input type="text" name="telefono" class="form-control" placeholder="Teléfono">
+                        </div>
+                        <div class="d-grid mt-3">
+                            <button id="membresia-btn" class="btn btn-outline-secondary"><i class="icon-ok"></i> Enviar solicitud</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional: Place to the bottom of scripts -->
+    <script>
+        const myModal = new bootstrap.Modal(document.getElementById('modalId'), options)
+    </script>
     <footer>
         <?php include('footer.php'); ?>
     </footer>
